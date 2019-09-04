b06d5b450dbe7ad8461d9e2efb0cf7b03baeee74
Iago Gomes <iago.barreto@dellead.com>
2019-09-04 12:24:20 -0300

    Configurando página de perfil do usuário

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../models/models';
+import { UserService } from '../services/user/user.service';
+import { AlertService } from '../services/alert/alert.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,5 +9,35 @@ import { User } from '../models/models';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  
+
+  private profileUser: User = {
+    usuario_id: null,
+    avatar: null,
+    nome: null,
+    cpf: null,
+    telefone: null,
+    email: null,
+    senha: null,
+    departamento: null,
+    eventos: [],
+    carga_horaria: null,
+    data_ingresso: null,
+    criado_em: null,
+    atualizado_em: null
+  }
+
+  constructor(
+    private userService: UserService,
+    private alertService: AlertService
+  ) {
+    this.userService.getId();
+    this.userService.getToken();
+  }
+
+  ngOnInit(){
+    this.userService.loadUser().subscribe( 
+      resp =>{ this.profileUser = resp },
+      erro => { this.alertService.presentToast(erro.message, 'danger');}
+    )
+  }
 }
